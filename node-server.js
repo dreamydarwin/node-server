73ed94939bf8669d74805d38080e673a80710eea
Berk Erol <berk-erol@outlook.com>
2021-10-29 21:35:06 +0300

    Add app

--- node-server.js
+++ node-server.js
@@ -2,10 +2,12 @@ const http = require('http');
 
 console.log('Node server starting on 8080');
 
-http.createServer((req, res) => {
+let app = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('Hello World!');
   res.end();
-}).listen(8080);
+});
+
+app.listen(8080);
 
 console.log('Node server running on 8080');
